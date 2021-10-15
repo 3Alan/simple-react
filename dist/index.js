e5e1396e99968d4c6870b73f509a64fb4fe2b99a
paulw <paulw@myautobest.com>
2021-10-15 18:31:40 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+var _jsxFileName = "D:\\github\\simple-react\\lib\\index.js";
+
+/** @jsx SimpleReact.createElement */
+const element = SimpleReact.createElement("div", {
+  __self: this,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 3,
+    columnNumber: 3
+  }
+}, SimpleReact.createElement("h1", {
+  __self: this,
+  __source: {
+    fileName: _jsxFileName,
+    lineNumber: 4,
+    columnNumber: 5
+  }
+}, "Hello React"));
+const container = document.getElementById('root');
+SimpleReact.render(element, container);
---
+++ open.js
@@ -0,0 +1,6 @@
+const open = require('open');
+const path = require('path');
+
+const page = path.resolve(__dirname, 'index.html');
+
+open(page);
